d3bed6736aea0779e65e594d4eb3d7bd9a9d177d
Mgrga97 <marko.grg97@gmail.com>
2024-12-06 20:07:07 +0100

    rjeseni E02Z1 i E03Z1 zadaci

---
+++ Ucenje/E05UvjetnoGrananjeSwitch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E05UvjetnoGrananjeSwitch
+    {
+
+
+        public static void Izvedi()
+        {
+
+            // Console.WriteLine("e05");
+
+            int i = 2;
+
+            switch (i)
+            {
+                case 1:
+                    Console.WriteLine("nedovoljan");
+                    break;
+                case 2:
+                    Console.WriteLine("dovoljan");
+                    break;
+                default:
+                    Console.WriteLine("ostalo");
+                    break;
+
+            }
+
+
+
+            char znak = '@';
+            Console.WriteLine("znak");
+            Console.WriteLine((int)znak);
+
+
+
+        }
+    }
+}
---
+++ Ucenje/E06Nizovi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E06Nizovi
+    {
+        public static void Izvedi()
+        {
+            // Console.WriteLine("e06");
+
+            int[] temp = new int[12];
+
+            temp[0] = 2; // siječanj
+            temp[11] = 4; // prosinac
+
+            Console.WriteLine(temp);
+            Console.WriteLine(string.Join(",", temp));
+
+
+            int[,] tablica =
+                {
+                {1,2,3},
+                {4,5,6 },
+                {7,8,9},
+
+            };
+
+            Console.WriteLine(tablica[1,2]);
+
+            int[,,] kocka = { };
+
+            int[,,,,,,] multiverse = { };
+
+
+
+        }
+
+    }
+}
